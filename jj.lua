43116791be942ad07dfca392827c301db4760fc3
pijiaobenMSJMleng <3868559297@qq.com>
2025-02-02 13:48:27 +0800

    Add files via upload

---
+++ jj.lua
@@ -0,0 +1,13 @@
+if ID == "e7ed410f-542f-4c7c-9241-6c23fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c23fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c23fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c23fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c23fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c23fbb379e0" then 
+JM = true 
+end
