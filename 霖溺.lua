ce23f9f16a2e9ffbf284bb8276502547583f6eb4
pijiaobenMSJMleng <3868559297@qq.com>
2025-02-02 13:38:59 +0800

    Add files via upload

---
+++ 霖溺.lua
@@ -0,0 +1,11 @@
+if ID == "e7ed410f-542f-4c7c-9241-6c93fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c93fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c93fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c93fbb379e0" then 
+JM = true 
+elseif ID == "e7ed410f-542f-4c7c-9241-6c93fbb379e0" then 
+JM = true 
+end
